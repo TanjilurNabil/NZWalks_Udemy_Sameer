c5bc20287eb051a33ef10dc791d8d0e971cebfc1
Tanjilur Rahman Nabil <tanjilur.nabil@savethechildren.org>
2024-02-04 16:32:32 +0600

    Region create get

---
+++ NZWalks.API/Controllers/RegionsController.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Data;
+using NZWalks.API.Models.Domain;
+using NZWalks.API.Models.DTO;
+using static System.Net.WebRequestMethods;
+
+namespace NZWalks.API.Controllers
+{
+    //https://localhost:1234/api/regions
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+    {
+        private readonly NZWalksDbContext dbContext;
+
+        public RegionsController(NZWalksDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        //Get all region
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            //Get Data from database - Domain Models
+            var regions = dbContext.Regions.ToList();
+            //Map Domain to DTO's
+            var regionsDto = new List<RegionDto>();
+            foreach (var region in regions)
+            {
+                regionsDto.Add(new RegionDto()
+                {
+                    Id = region.Id,
+                    Name = region.Name,
+                    Code = region.Code,
+                    RegionImageUrl = region.RegionImageUrl,
+                });
+            }
+            //Return DTO's
+            return Ok(regionsDto);
+        }
+        //Get Region by Id
+        [HttpGet]
+        [Route("{id:Guid}")]
+        public IActionResult GetById([FromRoute] Guid id) 
+        {
+            //var region = dbContext.Regions.Find(id); 
+            var region = dbContext.Regions.FirstOrDefault(x=>x.Id == id);
+            if(region== null)
+            {
+                return NotFound();
+            }
+            var regionDto = new RegionDto()
+            {
+                Id = region.Id,
+                Name = region.Name,
+                Code = region.Code,
+                RegionImageUrl = region.RegionImageUrl,
+            };
+            
+            return Ok(regionDto);
+        }
+        //POST To create New Region
+        [HttpPost]
+        public IActionResult Create([FromBody] AddRegionRequestDto addRegionRequestDto)
+        {
+            //Map DTo to Domain
+            var regionDomainModel = new Region
+            {
+                Name = addRegionRequestDto.Name,
+                Code = addRegionRequestDto.Code,
+                RegionImageUrl = addRegionRequestDto.RegionImageUrl
+            };
+            //Create Region
+            dbContext.Regions.Add(regionDomainModel);
+            dbContext.SaveChanges();
+            //Map Domain back to DTO
+            var regionDto = new RegionDto
+            {
+                Id = regionDomainModel.Id,
+                Name = regionDomainModel.Name,
+                Code = regionDomainModel.Code,
+                RegionImageUrl = regionDomainModel.RegionImageUrl
+            };
+            return CreatedAtAction(nameof(GetById), new { id = regionDomainModel.Id }, regionDto);
+        }
+    }
+}
--- NZWalks.API/Models/Domain/Walk.cs
+++ NZWalks.API/Models/Domain/Walk.cs
@@ -1,13 +1,13 @@
 ﻿namespace NZWalks.API.Models.Domain
 {
-    public class Walks
+    public class Walk
     {
         public Guid Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public double LengthInKm { get; set; }
         public string? WalkImageUrl { get; set; }
-        public Guid DificultyId { get; set; }
+        public Guid DifficultyId { get; set; }
         public Guid RegionId { get; set; }
 
         //Navigation Property.EF Core will provide necessary relationship according to this
